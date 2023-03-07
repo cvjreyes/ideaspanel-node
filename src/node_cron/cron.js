ef486ed768077f382b53c9e218b1a7c6f4e89474
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-03-07 16:40:38 +0100

    The ideas get published if they complete conditions every 24 hours

--- src/node_cron/cron.js
+++ src/node_cron/cron.js
@@ -1,15 +1,16 @@
 const cron = require("node-cron");
-const { checkDateFromIdeasValidate } = require("./ideas");
+const { checkDateFromIdeasValidate, ideasPublished } = require("./ideas");
 
 const cronFn = () => {
-    cron.schedule("* * 23 * *", () => {
-        checkDateFromIdeasValidate();
-        console.log(
-          new Date(Date.now()).toLocaleDateString(),
-          new Date(Date.now()).toLocaleTimeString(),
-          "Idea checked date"
-        );
-      });
+  cron.schedule("* * 23 * *", () => {
+    checkDateFromIdeasValidate();
+    ideasPublished();
+    console.log(
+      new Date(Date.now()).toLocaleDateString(),
+      new Date(Date.now()).toLocaleTimeString(),
+      "Idea checked date"
+    );
+  });
 };
 
 module.exports = () => {
--- src/node_cron/ideas.js
+++ src/node_cron/ideas.js
@@ -1,12 +1,11 @@
 const pool = require("../../config/db");
-const { getAllIdeasService } = require("../main/ideas/ideas.service");
 
 exports.checkDateFromIdeasValidate = async () => {
   try {
-    const [results] = await pool.query("SELECT * FROM ideas")
+    const [ideas] = await pool.query("SELECT * FROM ideas");
     const actualDate = new Date();
-    for (let i = 0; i < results.length; i++) {
-      const createdDate = new Date(results[i].sent_to_validate_at);
+    for (let i = 0; i < ideas.length; i++) {
+      const createdDate = new Date(ideas[i].sent_to_validate_at);
       const daysPassed = (
         (actualDate - createdDate) /
         1000 /
@@ -14,10 +13,43 @@ exports.checkDateFromIdeasValidate = async () => {
         60 /
         24
       ).toFixed();
-      if (daysPassed > 4) {
+      if (daysPassed > 14) {
         await pool.query(
-          "UPDATE ideas SET sent_to_validate = 0, draft = 1, sent_to_validate_at = NULL WHERE id = ?",
-          results[i].id
+          "UPDATE ideas SET sent_to_validate = 0, draft = 0 WHERE id = ?",
+          ideas[i].id
+        );
+      }
+    }
+  } catch (err) {
+    console.error(err);
+    throw err;
+  }
+};
+
+exports.ideasPublished = async () => {
+  try {
+    // Suma de los usuarios que son comittee
+    const [comitteeUsers] = await pool.query(
+      "SELECT COUNT(*) as 'ComiteeUsers' FROM users WHERE isComitee = 1"
+    );
+    // Recogemos todas las ideas y las recorremos
+    const [ideas] = await pool.query("SELECT * FROM ideas");
+    for (let i = 0; i < ideas.length; i++) {
+      // Contamos la cantidad de votos positivos de cada idea
+      const [positiveVotes] = await pool.query(
+        "SELECT COUNT(*) as 'PositiveVotes' FROM votes WHERE approved = 1 AND idea_id = ? ",
+        ideas[i].id
+      );
+      // Si esa idea tiene mas votos positivos que la mitad de la suma de todos los usuarios que son comittee
+      if (
+        positiveVotes[0].PositiveVotes >=
+        (comitteeUsers[0].ComiteeUsers / 2).toFixed()
+      ) {
+        // Se publica y si se le asigna la fecha correspondiente actual.
+        console.log("entra");
+        await pool.query(
+          "UPDATE ideas SET sent_to_validate = 0, published = 1, published_at = CURRENT_TIMESTAMP WHERE id = ?",
+          ideas[i].id
         );
       }
     }
