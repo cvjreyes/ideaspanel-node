6dd63dbaef7989796441652f697cc72a41e841e0
Sean Sáez Fuller <sean.saez.f@gmail.com>
2023-03-10 14:21:36 +0100

    idea_votes implemented

--- src/main/comittee_votes/comittee_votes.controller.js
+++ src/main/comittee_votes/comittee_votes.controller.js
@@ -10,7 +10,7 @@ exports.submitComitteeVote = async (req, res) => {
     const alreadyVoted = await checkIfComitteeUserAlreadyVotedIdea(user_id, idea_id);
     if (alreadyVoted) return send(res, true);
     const ok = await submitComitteeVoteService(idea_id, user_id, vote);
-    if (ok) return send(res, true, "Vote successfully done");
+    if (ok) return send(res, true, "Comittee vote successfully done");
   } catch (err) {
     console.error(err);
     send(res, false, err);
--- src/main/idea_votes/idea_votes.controller.js
+++ src/main/idea_votes/idea_votes.controller.js
@@ -1,16 +1,39 @@
 const { send } = require("../../helpers/send");
 const {
   submitIdeaVoteService,
-  checkIfUserAlreadyVotedIdea,
+  checkIfUserAlreadyVotedIdeaService,
+  getIdeaVotesService,
 } = require("./idea_votes.service");
+
+exports.getIdeaVotes= async (req, res) => {
+  const { idea_id } = req.params;
+  try {
+    const idea_votes = await getIdeaVotesService(idea_id);
+    send(res, true, idea_votes);
+  } catch (err) {
+    console.error(err);
+    send(res, false, err);
+  }
+};
+
+exports.checkIfUserAlreadyVotedIdea = async (req, res) => {
+  const { user_id, idea_id } = req.body;
+  try {
+    const alreadyVoted = await checkIfUserAlreadyVotedIdeaService(user_id, idea_id);
+    send(res, true, alreadyVoted);
+  } catch (err) {
+    console.error(err);
+    send(res, false, err);
+  }
+};
 
 exports.submitIdeaVote = async (req, res) => {
   const { idea_id, user_id, vote } = req.body;
   try {
-    const alreadyVoted = await checkIfUserAlreadyVotedIdea(user_id, idea_id);
+    const alreadyVoted = await checkIfUserAlreadyVotedIdeaService(user_id, idea_id);
     if (alreadyVoted) return send(res, true);
     const ok = await submitIdeaVoteService(idea_id, user_id, vote);
-    if (ok) return send(res, true, "Vote successfully done");
+    if (ok) return send(res, true, "Idea vote successfully done");
   } catch (err) {
     console.error(err);
     send(res, false, err);
