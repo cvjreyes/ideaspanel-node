c68a1e41e0cc87fc05b43d43b2c51b3d76b3a075
Toni <webdevtoni@gmail.com>
2023-03-10 10:31:36 +0100

    fixed voting multiple times and accessing comittee if not comittee

--- src/main/votes/votes.service.js
+++ src/main/votes/votes.service.js
@@ -1,10 +1,18 @@
 const pool = require("../../../config/db");
 
+exports.checkIfUserAlreadyVotedIdea = async (user_id, idea_id) => {
+  const [vote] = await pool.query(
+    "SELECT * FROM votes WHERE user_id = ? AND idea_id = ?",
+    [user_id, idea_id]
+  );
+  return vote[0];
+};
+
 exports.submitVoteService = async (idea_id, user_id, vote) => {
   const [finalVote] = await pool.query(
     "INSERT INTO votes (idea_id, user_id, approved) VALUES (?, ?, ?)",
     [idea_id, user_id, vote]
   );
-  
+
   return finalVote;
 };
