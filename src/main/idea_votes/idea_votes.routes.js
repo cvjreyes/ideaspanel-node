63d1f7f2b731dea47e07d0c1d72399d951329205
Toni <webdevtoni@gmail.com>
2023-03-10 14:48:03 +0100

    enfin

--- src/main/idea_votes/idea_votes.routes.js
+++ src/main/idea_votes/idea_votes.routes.js
@@ -2,6 +2,14 @@ const router = require("express").Router(),
   controller = require("./idea_votes.controller"),
   { checkAuth } = require("../../middlewares/checkAuth");
 
+router.get("/get_idea_votes/:idea_id", checkAuth, controller.getIdeaVotes);
+
+router.get(
+  "/check_user_idea_vote",
+  checkAuth,
+  controller.checkIfUserAlreadyVotedIdea
+);
+
 router.post("/submit_idea_vote", checkAuth, controller.submitIdeaVote);
 
 module.exports = router;
